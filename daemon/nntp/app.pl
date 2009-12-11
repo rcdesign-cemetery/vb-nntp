60cefee4368ed7a1b826719cffdd176008ef889e
Hibin <dmitry@wildev.ru>
2009-12-11 14:55:31 +0300

    Example config file renamed

--- daemon/nntp/app.pl
+++ daemon/nntp/app.pl
@@ -13,6 +13,9 @@
   my $Toolkit = Wildev::AppServer::Toolkit->instance();
      $Toolkit->Config = './app.cnf';
 
+     # to get settings from database we must force reload config here
+     $Toolkit->Config->ForceReload();
+
   my $Server  = Wildev::AppServer::Core->new();
      $Server->run();
 
