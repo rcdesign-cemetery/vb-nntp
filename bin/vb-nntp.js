d5a217a97755cc158edc51dfb80c865773028b45
Aleksey V Zapparov <ixti@member.fsf.org>
2011-11-17 00:10:40 +0100

    Implemented some of the commands

--- bin/vb-nntp.js
+++ bin/vb-nntp.js
@@ -26,6 +26,13 @@ var fs = require('fs'),
 var CONFIG_FILE = require('fs').realpathSync() + '/config.yml';
 
 
+function parseListenString(binding) {
+  binding = binding.split(':');
+  return (1 === binding.length) ? {port: +binding[0]}
+                                : {host: binding[0], port: +binding[1]};
+}
+
+
 // starts master app
 function startMaster() {
   var options = require(CONFIG_FILE).shift(),
@@ -38,8 +45,9 @@ function startMaster() {
 
 // starts worker app
 function startWorker() {
-  var options, logger, database, commander, servers = [], server;
+  var options, logger, database, commander, servers;
 
+  servers   = [];
   options   = require(CONFIG_FILE).shift();
   logger    = vbnntp.logger.createSlave(process);
   database  = vbnntp.database.create(options.database);
@@ -47,18 +55,22 @@ function startWorker() {
 
   // start plain server
   if (options.listen) {
-    server = vbnntp.initServer(vbnntp.nntp.Server, options, logger, database, commander);
-    servers.push(server.listen(options.listen));
-    logger.info('SERVER Listening', {binding: options.listen});
+    (function (bind) {
+      var server = vbnntp.initServer(vbnntp.nntp.Server, options, logger, database, commander);
+      servers.push(server.listen(bind.port, bind.host));
+      logger.info('SERVER Listening on', bind);
+    }(parseListenString(options.listen)));
   }
 
   // start secure server
   if (options.listen_ssl) {
-    // prepare options
-    options.key = options.cert = fs.readFileSync(options.pem_file);
-    server = vbnntp.initServer(vbnntp.nntps.Server, options, logger, database, commander);
-    servers.push(server.listen(options.listen_ssl));
-    logger.info('SERVER Listening', {binding: options.listen_ssl});
+    (function (bind) {
+      // prepare options
+      options.key = options.cert = fs.readFileSync(options.pem_file);
+      var server = vbnntp.initServer(vbnntp.nntps.Server, options, logger, database, commander);
+      servers.push(server.listen(bind.port, bind.host));
+      logger.info('SERVER Listening on', bind);
+    }(parseListenString(options.listen_ssl)));
   }
 }
 
